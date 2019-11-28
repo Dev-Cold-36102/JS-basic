a5832978d38a5d048320ada38012b974819243b5
Dev.Cold <devcolduet@gmail.com>
2019-11-28 14:23:00 +0700

    add file conan.html conan.js

--- conan.js
+++ conan.js
@@ -1,4 +1,37 @@
 function upArrowPressed() {
-    let element = document.getElementById("nobita");
-    element.style.top = parseInt(element.style.top) - 5 + 'px';
+    let element = document.getElementById("connan");
+    element.style.top = parseInt(element.style.top) - 10 + 'px';
+}
+function downArrowPressed() {
+    let element = document.getElementById("connan");
+    element.style.down = parseInt(element.style.top) +10 +'px'
+}
+function leftArrowPressed() {
+    let element = document.getElementById("connan");
+    element.style.left = parseInt(element.style.left) -10 +'px'
+}
+function rightArrowPressed() {
+    let element = document.getElementById("connan");
+    element.style.right = parseInt(element.style.left) +10 +'px'
+}
+function moveSelection(evt) {
+    switch (evt.keyCode) {
+        case 37:
+            leftArrowPressed();
+            break;
+        case 39:
+            rightArrowPressed();
+            break;
+        case 38:
+            upArrowPressed();
+            break;
+        case 40:
+            downArrowPressed();
+            break;
+    }
+}
+
+
+function docReady() {
+    window.addEventListener('keydown', moveSelection);
 }
