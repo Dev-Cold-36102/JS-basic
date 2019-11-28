1c0f79f7698c07ab9383d8e6450568d1b26c7f8b
Dev.Cold <devcolduet@gmail.com>
2019-11-28 16:18:03 +0700

    add file condition1.html condition.js

---
+++ condition1.js
@@ -0,0 +1,10 @@
+function exe1() {
+    let a=prompt("please enter a=");
+    let b=prompt("please enter b=");
+    if (a%b==0) {
+        alert("a chia het cho b");
+    } else {
+        alert("a khong chia het cho b");
+    }
+
+}
